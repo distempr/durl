99715173f143e8d3af45237dd1745284e194d80d
Harry Wheildon <harry.wheildon@gmail.com>
2024-03-16 15:50:34 +0000

    Enable write-ahead log on DB

--- durl.py
+++ durl.py
@@ -16,6 +16,7 @@ def get_db():
     db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(db_path)
+        db.execute("PRAGMA journal_mode=WAL")
     return db
 
 
